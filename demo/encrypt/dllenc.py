42ee76d78005736cd7ce8b483d926d03058118fa
Kyrios0 <1120626222@qq.com>
2018-08-03 10:02:40 +0800

    Update dll demo
    add basic code
    it will cause some error...

--- demo/encrypt/dllenc.py
+++ demo/encrypt/dllenc.py
@@ -1,19 +1,24 @@
+#-*- coding:utf-8 -*-
 import pefile
+import os
 
-pth = r"C:\Users\pzhxb\Desktop\kyrios_SGX\Github\SGX-Protect\demo\exetest\\"
-pe_name = 'restore.exe'
-pe_map = 'restore.map'
+pth = r'C:\Users\pzhxb\Desktop\kyrios_SGX\vs\enclave_hw2\Debug\\'
+pe_name = 'enclave_hw2.dll'
+pe_map = 'enclave_hw2.map'
+pe_xml = 'enclave_hw2.config.xml'
+key_file = 'enclave_hw2_private.pem'
 pe = pefile.PE(pth + pe_name)
 with open(pth + pe_map, 'rb') as f:
     binmap = f.readlines()
 
 enc_key = '123456'
 klen = len(enc_key)
-func_list = ['testFunc']
+func_list = ['_hello_sgx'] # To-Do: process _func_name
 offs_list = []
 
 # close base-addr randomization
-pe.OPTIONAL_HEADER.DllCharacteristics &= 0xff00
+# change this byte will cause sgx_create_enclave return 1
+# pe.OPTIONAL_HEADER.DllCharacteristics &= 0xff00
 
 # set .text section writable
 for section in pe.sections:
@@ -39,4 +44,9 @@ for offs in offs_list:
         enc_byte = ord(byte)^ord(enc_key[idx%klen])
         pe.set_bytes_at_offset(offs[0]+idx, bytes(chr(enc_byte)))
 
-pe.write(filename='enc.exe')
+pe.write(filename='enc.dll')
+
+# sign enc enclave 
+# `sgx_sign sign -enclave enclave.dll -config config.xml -out enclave_signed.dll -key private.pem`
+out_name = pe_name.replace('.', '.signed.')
+os.system('sgx_sign sign -enclave enc.dll -config %s -out %s -key %s' % (pe_xml, out_name, key_file))
