cd3243ada5b609b8cf8cbb3f56c904d49c0bee3e
Kyrios0 <1120626222@qq.com>
2019-05-17 16:45:08 +0800

    Update dllenc.py

--- encrypt/dllencrypt.py
+++ encrypt/dllencrypt.py
@@ -2,7 +2,7 @@
 import pefile
 import os
 
-pth = r'C:\Users\pzhxb\Desktop\kyrios_SGX\Github\SGX-Protect\\'
+pth = r'C:\Users\kira\Desktop\\'
 pe_name = 'enclave_demo.dll'
 pe_map = 'enclave_demo.map'
 pe_xml = 'enclave_demo.config.xml'
@@ -11,7 +11,7 @@ pe = pefile.PE(pth + pe_name)
 with open(pth + pe_map, 'rb') as f:
     binmap = f.readlines()
 
-enc_key = '123456'
+enc_key = '*'
 klen = len(enc_key)
 with open('SGXWhiteList.txt') as f:
     func_list = [line.split('\n')[0] for line in f.readlines()]
@@ -23,6 +23,7 @@ offs_list = []
 # pe.OPTIONAL_HEADER.DllCharacteristics &= 0xff00
 
 # set .text section writable
+print 'Setting write permission.'
 for section in pe.sections:
     if section.Name == ".text\x00\x00\x00":
         section.Characteristics |= 0x80000000 # #define IMAGE_SCN_MEM_WRITE 0x80000000 // Section is writeable.
@@ -30,6 +31,7 @@ for section in pe.sections:
         break
 
 # extract function address
+print 'Extracting function address.'
 for idx, line in enumerate(binmap): # To-Do: speed optimize
     line = line.split()
     if len(line) < 2:
@@ -40,9 +42,11 @@ for idx, line in enumerate(binmap): # To-Do: speed optimize
             end = text_base + int(binmap[idx+1].split()[0].split(':')[1], 16)
             offs_list.append((start, end))
 
+print 'Sanitizing...'
 for func_id, offs in enumerate(offs_list):
     enc_bytes = []
-    func_bytes = pe.get_memory_mapped_image()[offs[0]+0xc00:offs[1]+0xc00] # why 0xc00?
+    func_bytes = pe.get_memory_mapped_image()[offs[0]+0xa00:offs[1]+0xa00] # why 0xa00?
+    # print ['%02x' % ord(i) for i in func_bytes[:10]]
     for idx, byte in enumerate(func_bytes):
         enc_byte = ord(byte)^ord(enc_key[idx%klen])
         enc_bytes.append(chr(enc_byte))
